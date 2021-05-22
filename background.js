a213fb0d549a62bae9e0bd639e2f8ae8d8e85320
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-05-22 18:14:37 +0530

    fix

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 const defaultFilters = [
     "*://*.facebook.com/*",
  "*://*.youtube.com/*",
+ "*://*.instagram.com/*",
 ]
 chrome.webRequest.onBeforeRequest.addListener(
     function(details){ return {cancel:true}},
